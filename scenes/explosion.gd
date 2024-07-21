40a0e65f652b45d59b13093a03922b7175c63389
Kalle Fagerberg <kalle.fagerberg@riskident.com>
2024-07-21 18:33:58 +0200

    More sounds

--- scenes/explosion.gd
+++ scenes/explosion.gd
@@ -8,4 +8,6 @@ func _ready():
 	var tween = get_tree().create_tween()
 	scale = Vector3.ONE * scale_start
 	tween.tween_property(self, "scale", Vector3.ONE * scale_end, scale_time_seconds)
+	tween.tween_property(self, "visible", false, 0)
+	tween.tween_interval(0.4)
 	tween.tween_callback(queue_free)
